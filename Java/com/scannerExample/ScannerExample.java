7fd2b92dd24e36b75322a49cb6c04402df0468d4
AnnapooraniBabu <poojasri92749@gmail.com>
2022-05-02 15:24:59 +0530

    Scannerclass example

---
+++ Java/com/scannerExample/ScannerExample.java
@@ -0,0 +1,37 @@
+package com.scannerExample;
+import java.util.Scanner;
+public class ScannerExample {
+    public static void main(String[] args){
+    Scanner obj = new Scanner(System.in);
+
+    //              NEXTLINE()
+    System.out.println("Enter your text : ");
+    String text = obj.nextLine();
+    System.out.println("The text is "+text);
+
+
+    //               NEXTBYTE()
+    System.out.println("Enter your age!");
+    byte age = obj.nextByte();
+    System.out.println("The age is "+age);
+
+
+    //               NEXTINT()
+    System.out.println("Please enter your Phone number!");
+    int phoneNo = obj.nextInt();
+    System.out.println("The phone number is "+phoneNo);
+
+
+    //               NEXT()
+    System.out.println("Enter your name : ");
+    String name = obj.next();
+    System.out.println("The name is  "+name);
+
+
+    //             nextBoolean()
+    System.out.println("Are you 18+?");
+    boolean bn = obj.nextBoolean();
+    System.err.println("The age is "+ bn);
+
+    }
+}
