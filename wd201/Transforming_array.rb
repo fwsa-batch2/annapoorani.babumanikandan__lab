7768fbfed126afbb727ad5fcefdc3632f2bc1348
AnnapooraniBabu <poojasri92749@gmail.com>
2022-06-19 22:51:22 +0530

    Completed the Transforming an array task

---
+++ wd201/Transforming_array.rb
@@ -0,0 +1,2 @@
+num =[1,2,3,4,5,6,7]
+num.each{|p|puts "The current number is #{p}"}
---
+++ wd201/Variables.rb
@@ -0,0 +1,23 @@
+name ="poorani"
+age = 18
+puts "Hi all! I'm "+ name +" and my age is " +age.to_s
+
+#String Interpolation
+
+nic_name = "Bunny"
+puts "Happy day from #{nic_name}"
+
+# Integer
+num=[15,10,23,27,4,9]
+puts "#{num.sum/num.length.to_f}"
+
+# String
+str="all is well"
+puts str.length
+
+puts str.upcase
+
+txt = "ALl is Well"
+puts txt.downcase
+
+puts str.capitalize
